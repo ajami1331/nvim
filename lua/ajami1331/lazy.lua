1a1d13d8c1bc551893e4edd9fb1efa96ae194e6d
Araf Al Jami <clown1331@yahoo.com>
2024-04-03 16:59:28 +0100

    Update

--- lua/ajami1331/lazy.lua
+++ lua/ajami1331/lazy.lua
@@ -13,7 +13,7 @@ vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
 {
-    'nvim-telescope/telescope.nvim', tag = '0.1.1',
+    'nvim-telescope/telescope.nvim', tag = '0.1.6',
     -- or                            , branch = '0.1.x',
     dependencies = { {'nvim-lua/plenary.nvim'} }
     };

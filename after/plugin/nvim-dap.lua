968b0be53b870ca751ac6e3863558c192e64999a
Araf Al Jami <clown1331@yahoo.com>
2024-04-04 07:55:06 +0100

    Update

--- after/plugin/nvim-dap.lua
+++ after/plugin/nvim-dap.lua
@@ -14,3 +14,18 @@ end)
 vim.keymap.set({'n', 'v'}, '<Leader>dp', function()
   require('dap.ui.widgets').preview()
 end)
+
+local dap = require('dap')
+dap.adapters.codelldb = {
+  type = 'server',
+  port = "1234",
+  executable = {
+    -- CHANGE THIS to your path!
+    command = 'codelldb',
+    args = { "--port", "1234" },
+
+    -- On windows you may have to uncomment this:
+    -- detached = false,
+  }
+
+}
